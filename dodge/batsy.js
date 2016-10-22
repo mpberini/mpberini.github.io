2b0d479563871af7e0784cb897227629a68d2307
Madison <mpberini@skilstak.sh>
2016-10-22 17:01:19 -0400

    save

--- dodge/batsy.js
+++ dodge/batsy.js
@@ -1,8 +1,11 @@
 class Boot {
   preload() {
-    
+   this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+   this.scale.pageAlignHorizontally = true;
+   this.scale.pageAlignVertically = true;
   }
 }
 
 var game = new Phaser.Game (568,320);
-
+game.state.add("Boot", Boot);
+game.state.start("Boot");
