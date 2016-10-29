ed3abc64a203f18aac5d444be9b7508eadb6d79f
Madison <mpberini@skilstak.sh>
2016-10-29 16:46:42 -0400

    save

--- dodge/batsy.js
+++ dodge/batsy.js
@@ -17,8 +17,16 @@ class Load {
     console.log("Loading Complete.");
   }
 }
+class Play {
+  create() {
+    console.log("Entered Play State");
+    this.add.tileSprite(0,0,320,568,"bg");
+    this.background.autoScroll(0,700);
+  }
+}
 
-var game = new Phaser.Game (568,320);
+var game = new Phaser.Game(568,320);
 game.state.add("Boot",Boot);
 game.state.add("Load",Load);
+game.state.add("Play",Play);
 game.state.start("Boot");
